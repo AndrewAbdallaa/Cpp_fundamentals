efd55645504209814b6bb7f239c5acb3d1cf66e4
andrew <andrew.abdallaa@gmail.com>
2023-04-27 22:23:40 +0200

    10

---
+++ variables_naming_rules/README.md
@@ -0,0 +1,13 @@
+Variables Naming Rules & Best Practices
+
+  - Naming Rules
+  --- Must Be Unique
+  --- Case Sensitive
+  --- Cannot Start With Numbers
+  --- Nums Or Letters Or Underscore
+  --- No White Space Or Special Characters
+  --- Reserved Keywords "Class, Public"
+
+  - Best Practices
+  --- Related Names
+  --- Writing Style
---
+++ variables_naming_rules/Video_10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int price = 100; // Declare A New Variable
+  price = 200;     // Update The Value
+  int Price = 400;
+  cout << price;
+  cout << "\n===============\n";
+  int n1um1111 = 10;
+  cout << n1um1111;
+  cout << "\n===============\n";
+  int _num_bers_ = 100;
+  cout << _num_bers_;
+  cout << "\n===============\n";
+  int publics = 1000;
+  cout << publics;
+  cout << "\n===============\n";
+  int OSAMA = 500; // Bad Style
+  cout << OSAMA;
+  cout << "\n===============\n";
+  int x = 1000;
+  cout << x;
+  return 0;
+}
