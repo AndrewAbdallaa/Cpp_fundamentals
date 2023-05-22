0280b603ce217bd17475a831bfa834e8a730bd63
AndrewAbdallaa <122892548+AndrewAbdallaa@users.noreply.github.com>
2023-05-22 11:27:34 +0300

    check

---
+++ array-access_elements_&_memory_location/Video_39.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nums[]{100, 200, 300};
+
+    cout << "First Element: " << nums[0] << "\n";
+    cout << "Last Element: " << nums[2] << "\n"; // Number Of Elements - 1
+}
